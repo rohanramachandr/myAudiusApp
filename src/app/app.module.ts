2e0a0c2fa2d81cc6684f9f062c7b2814f16714b1
Rohan Ramachandran <rmramachandr@wisc.edu>
2021-07-24 16:48:33 -0500

    created header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbdCollapseNavbar } from './collapse-navbar';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgbdCollapseNavbar
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
