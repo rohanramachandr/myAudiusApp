70bd9b86ba43e600059b0ab02b285672270ac1d6
Rohan Ramachandran <rmramachandr@wisc.edu>
2021-09-23 13:58:56 -0500

    deploying cover letter site

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'myGallifyApp'`, () => {
+  it(`should have as title 'myTwilioApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     console.log("title", app.title);
